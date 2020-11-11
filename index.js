fed6aa2095df8128ba09d15b17f4b2d95e934393
baarosh <norbert.lipinski96@gmail.com>
2020-11-11 23:58:33 +0100

    added logic for doodler

--- index.js
+++ index.js
@@ -43,28 +43,28 @@ const createPlatforms = () => {
 
 const movePlatforms = () => {
         movePlatformsId = setInterval(() => {
-            if(doodler.bottom > 0) {
+            if(doodler.bottom > 10) {
                 platforms.forEach((platform) => {
-                    platform.bottom -= 5
+                    platform.bottom -= 1
                     platform.visual.style.bottom = `${platform.bottom}px`
             })
             } else clearInterval(movePlatformsId)
-        }, 80)
+        }, 10)
 }
 
-const moveDoodlerUp = () => {
+const moveDoodlerUp = (jumpHeight = 400) => {
         clearInterval(moveDoodlerDownId)
 
         moveDoodlerUpId = setInterval(() => {
-            if(doodler.bottom < 500) {
+            if(doodler.bottom < jumpHeight) {
                 isJumping = true
-                doodler.bottom += 20
+                doodler.bottom += 5
                 doodler.visual.style.bottom = `${doodler.bottom}px`
             } else {
                 clearInterval(moveDoodlerUpId)
                 moveDoodlerDown()
             }
-        }, 60)
+        }, 10)
 }
 
 const moveDoodlerDown = () => {
@@ -73,24 +73,80 @@ const moveDoodlerDown = () => {
         moveDoodlerDownId = setInterval(() => {
             if (doodler.bottom > 0) {
                 isJumping = false
-                doodler.bottom -= 20
+                doodler.bottom -= 5
                 doodler.visual.style.bottom = `${doodler.bottom}px`
             } else {
                 clearInterval(moveDoodlerDownId)
+                clearInterval(moveLeftId)
+                clearInterval(moveRightId)
                 console.log('game over')
             }
 
             platforms.forEach(platform => {
-                if ((doodler.bottom >= (platform.bottom))
+                if ((doodler.bottom >= (platform.bottom + 10))
                 &&  (doodler.bottom <= platform.bottom + 20)
                 &&  ((doodler.left + 40) >= platform.left)
                 &&  (doodler.left <= (platform.left + 120))
                 &&  !isJumping) {
                         console.log('landed')
-                        moveDoodlerUp()
+                        moveDoodlerUp(platform.bottom + 300)
                     }
             })
-    }, 60)
+    }, 10)
+}
+
+    const moveLeft = () => {
+    if(isRightJumping) {
+        clearInterval(moveRightId)
+        isRightJumping = false
+        }
+    if(!isLeftJumping){
+        moveLeftId = setInterval(() => {
+            if (doodler.left > 0 && doodler.bottom > 0) {
+                isLeftJumping = true
+                doodler.left -= 5
+                doodler.visual.style.left = `${doodler.left}px`
+            } else if (doodler.bottom > 0) {
+                moveRight()
+
+            } else clearInterval(moveLeftId)
+            }, 10)
+    }
+    }
+const moveRight = () => {
+    if(isLeftJumping) {
+        clearInterval(moveLeftId)
+        isLeftJumping = false
+    }
+    if(!isRightJumping) {
+        moveRightId = setInterval(() => {
+            if (doodler.left < 460 && doodler.bottom > 0) {
+                isRightJumping = true
+                doodler.left += 5
+                doodler.visual.style.left = `${doodler.left}px`
+            } else if (doodler.bottom > 0) {
+                moveLeft()
+
+            } else clearInterval(moveRightId)
+
+        }, 10)
+    }
+}
+
+const control = (e) => {
+    if (e.key === 'ArrowLeft' && doodler.bottom > 0) {
+         moveLeft()
+    }
+    else if (e.key === 'ArrowRight' && doodler.bottom > 0) {
+        moveRight()
+
+}
+    else if (e.key === 'ArrowUp' && doodler.bottom > 0) {
+        isLeftJumping = false
+        isRightJumping = false
+        clearInterval(moveLeftId)
+        clearInterval(moveRightId)
+}
 }
 
 let gameRunning = false
@@ -100,6 +156,10 @@ let movePlatformsId
 let moveDoodlerUpId
 let moveDoodlerDownId
 let isJumping
+let moveLeftId
+let moveRightId
+let isLeftJumping = false
+let isRightJumping = false
 
 const doodler = new Doodler()
 
@@ -109,6 +169,7 @@ const startGame = () => {
         movePlatforms()
         doodler.refresh(platforms[0].left)
         moveDoodlerUp()
+        document.addEventListener('keyup', control)
     }
 }
 
